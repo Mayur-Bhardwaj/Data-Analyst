9cf14da3274c3279d8fdfaa57c6315137feb4aff
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-05-18 15:09:55 +0000

    Add Files

---
+++ 1.py
@@ -0,0 +1,25 @@
+a=10;
+print(a);  #10
+
+print(id(a));    #The value 10 is store here --> 97192111851720
+
+#Types of variable --> numeric, string, float(decimal), Boolean Variables(true, false).
+
+numeric = 20;
+print(numeric);  #20
+
+#String
+
+text = "This is python";
+print(text);        #This is python
+
+#Float
+
+flo = 23.5;
+print(flo);
+
+#Boolean
+
+bool = true;
+print(bool);
+
