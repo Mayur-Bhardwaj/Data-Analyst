2be0d5e01027e3605bbe3e23548f5e997cde7f56
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-05-19 17:49:31 +0530

    Update

---
+++ Python/UserInput.py
@@ -0,0 +1,36 @@
+#Find the age for Voting using User Input.
+
+
+# no1 = int(input("Enter your age"))
+# if(no1<18):
+#     print("Sorry! You are not Valid Voter")
+# else:
+#     print("Congrats!! You are eligible for giving Vote.")
+
+
+# Output:
+# Enter your age 24
+# Congrats!! You are eligible for giving Vote.
+
+n1 = int(input("Please Enter First Number"))
+n2 = float(input("Please Enter Second Number"))
+
+n1>n2
+
+# Please Enter First Number 24
+# Please Enter Second Number 56
+# False
+
+# Please Enter First Number 78
+# Please Enter Second Number 34
+# True
+
+
+n1 = int(input("Please Enter First Number"))
+n2 = float(input("Please Enter Second Number"))
+n1=n2
+n1==n2
+
+# Please Enter First Number 34
+# Please Enter Second Number 45
+# True
