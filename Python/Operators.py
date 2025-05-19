d1af3ea8448e825b43355061ce0cecf2cf230b2f
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-05-19 17:32:20 +0530

    Operators

---
+++ Python/Operators.py
@@ -0,0 +1,31 @@
+# Arithemtic Operator
+# a=10
+# b=20
+# print(a+b) # 30
+# print(a-b) # -10
+# print(a*b) # 200
+# print(a/b) # 0.5
+
+
+
+# a=7
+# b=3
+# print(a/b) # 2.3333333333333335
+
+# print(a//b)  # 2     # They gives the Quotient
+
+# print(a%b) # 1      # They gives the Remainder
+
+
+
+# Comparison Operator
+
+a = 13
+b = 33
+
+print(a > b)    # False
+print(a < b)    # True
+print(a == b)   # False
+print(a != b)   # True
+print(a >= b)   # False
+print(a <= b)   # True
