47dd062dd9960907fa2ec13e64f55e8e83a94b90
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-05-20 16:28:05 +0530

    Practice Questions

--- Python/Practice/SquareRoot.py
+++ Python/Practice/SquareRoot.py
@@ -1,1 +1,22 @@
+# Enter a number and finde square root
 
+numbers = int(input("Enter the number for Square Root"))
+
+sqrt = pow(numbers,0.5)
+
+print (f"The Square Root Of Given Number is {sqrt}")
+
+# Output:-
+# Enter the number for Square Root 49
+# The Square Root Of Given Number is 7.0
+
+# Another Method
+
+numbers = int(input("Enter the number"))
+sqrt2 = numbers ** 0.5
+
+print(f"The Square Root Of Given Number is {sqrt2}")
+
+# Output
+# Enter the number 25
+# The Square Root Of Given Number is 5.0
