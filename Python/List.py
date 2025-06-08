d9ac4e77e1b7520cfde9489fce99a62cf112258f
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-06-08 16:15:11 +0530

    List

--- Python/List.py
+++ Python/List.py
@@ -23,7 +23,7 @@ print(a[2:5])   # [3, 4, 5]
 print(a[1:3])   # [2, 3]
 
 print(a[2:])  # [3,4,5]
-# Here [2:] means  display va;ues from starting 2nd index to last index all print
+# Here [2:] means  display values from starting 2nd index to last index all print
 
 print(a[:4])  # [1, 2, 3, 4]
 # Display from starting to the 4-1 = 3rd index
@@ -43,12 +43,56 @@ print(b)        # [1, 2, 3, 4, 'Natraj', 6, 7, 8, 9, 10]
 print(id(b))    # 1650441598400
 
 b[6] = "Apsara"
-print(b)        # [1, 2, 3, 4, 'Natraj', 6, 'Apsara', 8, 9, 10]
+# print(b)        # [1, 2, 3, 4, 'Natraj', 6, 'Apsara', 8, 9, 10]
 print(id(b))    # 1650441598400
 
 # If we want to insert the value at any index here suppose we want to insert value at index 3.
 # By using the insert function we do it.
 
 b.insert(2,"Radhika")
-print(b)   # [1, 2, 'Radhika', 3, 4, 'Natraj', 6, 'Apsara', 8, 9, 10]
-    
+# print(b)  # [1, 2, 'Radhika', 3, 4, 'Natraj', 6, 'Apsara', 8, 9, 10]
+
+# Note:-  If we print the single value then it is called "Indexing" and if we print the bunch of then it is called "Slicing".
+
+# If we want to add the wnd of list then we use "append".
+
+print(b)
+print(id(b))
+
+b.append(24)
+print(b)  # [1, 2, 'Radhika', 3, 4, 'Natraj', 6, 'Apsara', 8, 9, 10, 24]
+
+b.append("Stop")
+print(b)    # [1, 2, 'Radhika', 3, 4, 'Natraj', 6, 'Apsara', 8, 9, 10, 24, 'Stop']
+
+b[0] = "Start"
+print(b)    # ['Start', 2, 'Radhika', 3, 4, 'Natraj', 6, 'Apsara', 8, 9, 10, 24, 'Stop']
+
+# Note: Changes at any place but the memory location will not be changed is called mutable.
+
+# Delete
+# There is 2 types
+
+print(b)
+# ['Start', 2, 'Radhika', 3, 4, 'Natraj', 6, 'Apsara', 8, 9, 10, 24, 'Stop']
+b.remove("Start")   # Remove the mentioned Element
+print(b)  # [2, 'Radhika', 3, 4, 'Natraj', 6, 'Apsara', 8, 9, 10, 24, 'Stop']
+
+
+b.pop(5)  # They are remove the particular vale at index no.
+print(b)  # [2, 'Radhika', 3, 4, 'Natraj', 'Apsara', 8, 9, 10, 24, 'Stop']
+
+# Now we want to sort the list 
+
+l1 = [23,89,45,65,58]
+l1.sort()   # They are used to arrange the list in ascending order.
+print(l1)    # [23, 45, 58, 65, 89]
+
+l1.sort(reverse=True)   # They are used to arrange the list in descending order.
+print(l1)  # [89, 65, 58, 45, 23]
+
+
+list_Reverse = ["HTML", "CSS", "JavaScript", "Python", "ReactJs", "NodeJS"]
+list_Reverse.reverse()  # To reverse the list
+print(list_Reverse)   # ['NodeJS', 'ReactJs', 'Python', 'JavaScript', 'CSS', 'HTML']
+
