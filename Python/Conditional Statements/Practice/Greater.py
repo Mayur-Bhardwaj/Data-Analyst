2194eea5da552f39ca7837d50a53d28353d74e5e
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-07-01 18:20:17 +0530

    Greater Number

---
+++ Python/Conditional Statements/Practice/Greater.py
@@ -0,0 +1,19 @@
+# Take 2 inputs from user and tells which number is greater.
+
+number_1 = int(input("Please Enter the First Number: "))
+number_2 = int(input("Please Enter the Second Number: "))
+
+if number_1 > number_2:
+    print(f"{number_1} is Greater than {number_2}")
+else:
+    print(f"{number_2} is Greater than {number_1}")
+
+
+# Output:
+# Please Enter the First Number: 34
+# Please Enter the Second Number: 45
+# 45 is Greater than 34
+
+# Please Enter the First Number: 87
+# Please Enter the Second Number: 26
+# 87 is Greater than 26
