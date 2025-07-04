e2ca334444899c100d864855815b2e40bbd420bd
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-07-04 18:30:28 +0530

    Even Number

---
+++ Python/Loops/EvenNumber.py
@@ -0,0 +1,45 @@
+# Write a program to find even number from 2 to till user Specified.
+
+number = int(input("Please Enter the number: "))
+i=1;
+while i<=number:
+    if(i %2 == 0):
+        print(i)
+    i=i+1
+
+
+# Output:
+# Please Enter the number: 10
+# 2
+# 4
+# 6
+# 8
+# 10
+
+
+# Please Enter the number: 50
+# 2
+# 4
+# 6
+# 8
+# 10
+# 12
+# 14
+# 16
+# 18
+# 20
+# 22
+# 24
+# 26
+# 28
+# 30
+# 32
+# 34
+# 36
+# 38
+# 40
+# 42
+# 44
+# 46
+# 48
+# 50
