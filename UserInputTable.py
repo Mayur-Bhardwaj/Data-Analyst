926c07a69f7ceebf41aafa3f1276004c2f195db3
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-07-04 18:09:06 +0530

    User Input Table

---
+++ UserInputTable.py
@@ -0,0 +1,53 @@
+
+# Take a user Input for which table to print
+
+table = int(input("Please Enter the number for print the Table: "))
+limit = int(input("Please Enter the Limit to print the Table: "))
+
+i = 1;
+while i<=limit:
+    print(f"{table} * {i} = {table*i}")
+    i = i+1
+
+
+# Output:
+# Please Enter the number for print the Table: 12
+# Please Enter the Limit to print the Table: 10
+# 12 * 1 = 12
+# 12 * 2 = 24
+# 12 * 3 = 36
+# 12 * 4 = 48
+# 12 * 5 = 60
+# 12 * 6 = 72
+# 12 * 7 = 84
+# 12 * 8 = 96
+# 12 * 9 = 108
+# 12 * 10 = 120
+
+
+# 5 * 1 = 5
+# 5 * 2 = 10
+# 5 * 3 = 15
+# 5 * 4 = 20
+# 5 * 5 = 25
+# 5 * 6 = 30
+# 5 * 7 = 35
+# 5 * 8 = 40
+# 5 * 9 = 45
+# 5 * 10 = 50
+# 5 * 11 = 55
+# 5 * 12 = 60
+# 5 * 13 = 65
+# 5 * 14 = 70
+# 5 * 15 = 75
+# 5 * 16 = 80
+# 5 * 17 = 85
+# 5 * 18 = 90
+# 5 * 19 = 95
+# 5 * 20 = 100
+# 5 * 21 = 105
+# 5 * 22 = 110
+# 5 * 23 = 115
+# 5 * 24 = 120
+# 5 * 25 = 125
+
