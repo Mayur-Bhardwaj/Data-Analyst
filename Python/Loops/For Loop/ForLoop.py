bfe949ba0a00e25043b0bb6b4b90025bbe57217d
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-07-05 17:02:14 +0530

    For Loop

--- Python/Loops/For Loop/ForLoop.py
+++ Python/Loops/For Loop/ForLoop.py
@@ -1,1 +1,35 @@
+# For Loop:- Python For Loops are used for iterating over a sequence like lists, tuples, strings, and ranges.
+#  Iterable objects are those objects that allows are to iterate/move over every element of itself.
+# Ex:- List(go in every element through indexing), tuple, dictionary(through key-value pair).
+#  Types of Iteerable Objects:- List, Tuple, Sets, Dictionary, Range, String.
 
+# Iterable Objects
+
+list1 = ["Python","Django","Flask","FastAPI"]
+
+for l in list1:
+    print(l)
+
+# Output:-
+# Python
+# Django
+# Flask
+# FastAPI
+
+list2 = ["Apple","Mango", "Orange", "Banana"]
+for l2 in list2:
+    print(l2)
+    
+print(list2[1:4])
+
+# Output:-
+# Apple
+# Mango
+# Orange
+# Banana
+# ['Mango', 'Orange', 'Banana']
+
+# For Loop
+
+
+  
